61ac0de317a1e9e17c861c7dcf541aa3cb42cc43
Yujingping <Mistral@weymire.com>
2016-06-14 22:13:53 +0800

    Camera Button and TestPhotoButton Attached.

---
+++ TestOnly/TestPhotoButton.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Logic of the script: Whenever the player hits the button, start counting time. When the finger is lifted up, 
+/// Determine what to do based on the time counted. 
+/// </summary>
+public class TestPhotoButton : MonoBehaviour 
+{
+	public float longPressTime;
+
+	private bool isFingerDown;
+
+	private float holdTime;
+
+	public void OnPress (bool state)
+	{
+		isFingerDown = state;
+		//If the Finger is lifted up from the screen, process the touch. 
+		if (!state)
+		{
+			if (holdTime >= longPressTime)
+			{
+				LongPress();
+			}
+			else
+				ShortPress();
+			holdTime = 0f;
+		}
+	}
+
+	void Update ()
+	{
+		if (isFingerDown)
+			holdTime += Time.deltaTime;
+	}
+
+	private void LongPress ()
+	{
+		Debug.Log("This is a long Press!");
+	}
+
+	private void ShortPress ()
+	{
+		Debug.Log("This is a short Press!");
+	}
+}
---
+++ View/CameraButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Please note that "CameraButton" is not only the button ... it also includes the background sprite (used to capture event) and 
+/// camera mode options.
+/// </summary>
+public class CameraButton : MonoBehaviour 
+{
+	public UISprite backGroundSprite;
+	public UIButton[] modeButtons;
+
+	private void Awake ()
+	{
+		
+	}
+
+	public void ActivateModeSelection()
+	{
+		
+	}
+
+	public void TakePhoto ()
+	{
+		
+	}
+}
